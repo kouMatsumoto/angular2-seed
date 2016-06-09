c472103621d68d17b0430b202dd52f4f898eca05
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2016-06-09 16:52:47 +0900

    Complete Tutorial6

--- app/app.component.js
+++ app/app.component.js
@@ -13,6 +13,7 @@ var heroes_component_1 = require('./heroes.component');
 var hero_service_1 = require('./hero.service');
 var router_deprecated_1 = require('@angular/router-deprecated');
 var dashboard_component_1 = require("./dashboard.component");
+var hero_detail_component_1 = require("./hero-detail.component");
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
@@ -27,7 +28,8 @@ var AppComponent = (function () {
                 router_deprecated_1.ROUTER_PROVIDERS,
                 hero_service_1.HeroService
             ],
-            template: "\n    <h1>{{title}}</h1>\n    <nav>\n      <a [routerLink]=\"['Dashboard']\">Dashboard</a>\n      <a [routerLink]=\"['Heroes']\">Heroes</a>\n    </nav>\n    <router-outlet></router-outlet>\n  "
+            template: "\n    <h1>{{title}}</h1>\n    <nav>\n      <a [routerLink]=\"['Dashboard']\">Dashboard</a>\n      <a [routerLink]=\"['Heroes']\">Heroes</a>\n    </nav>\n    <router-outlet></router-outlet>\n  ",
+            styleUrls: ['app/app.component.css']
         }),
         router_deprecated_1.RouteConfig([
             {
@@ -40,6 +42,11 @@ var AppComponent = (function () {
                 name: 'Dashboard',
                 component: dashboard_component_1.DashboardComponent,
                 useAsDefault: true
+            },
+            {
+                path: '/detail/:id',
+                name: 'HeroDetail',
+                component: hero_detail_component_1.HeroDetailComponent
             }
         ]), 
         __metadata('design:paramtypes', [])
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,7 @@ import {HeroesComponent} from './heroes.component';
 import {HeroService} from './hero.service';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import {DashboardComponent} from "./dashboard.component";
+import {HeroDetailComponent} from "./hero-detail.component";
 
 @Component({
   selector: 'my-app',
@@ -20,7 +21,8 @@ import {DashboardComponent} from "./dashboard.component";
       <a [routerLink]="['Heroes']">Heroes</a>
     </nav>
     <router-outlet></router-outlet>
-  `
+  `,
+  styleUrls: ['app/app.component.css']
 })
 @RouteConfig([
   {
@@ -33,6 +35,11 @@ import {DashboardComponent} from "./dashboard.component";
     name: 'Dashboard',
     component: DashboardComponent,
     useAsDefault: true
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
   }
 ])
 export class AppComponent {
